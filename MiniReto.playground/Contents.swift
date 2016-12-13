7064012713478b424ca3c0ad32e2c2c3996f0ee3
enmanuelcid <enmanuel.cid@gmail.com>
2016-12-13 12:43:38 -0400

    enhancements for MiniReto
    arreglo de las validaciones para combinar posibles combinaciones en el mismo numero

--- MiniReto.playground/Contents.swift
+++ MiniReto.playground/Contents.swift
@@ -9,16 +9,20 @@ for numero in numeros {
     case 30 ... 40:
         if numero % 5 == 0 {
             print( "\(numero)" + " Bingo!" + " Viva Swift!")
-        } else if numero % 5 == 0 || numero % 2 == 0{
+        } else if numero % 5 == 0 && numero % 2 == 0{
+            print("\(numero)" + " Bingo!" + " Par" + " Viva Swift!")
+        } else if numero % 2 == 0 {
             print("\(numero)" + " Bingo!" + " Par" + " Viva Swift!")
         } else {
-            print("\(numero)" + " Impar" + " Viva Swift!")
+            print("\(numero)" + " Bingo!"  + " Impar" + " Viva Swift!")
         }
     default:
-        if numero % 5 == 0 {
-            print( "\(numero)" + " Bingo!")
-        } else if numero % 5 == 0 || numero % 2 == 0{
-            print("\(numero)" + " Bingo!" + " Par")
+        if numero % 5 == 0 && numero % 2 == 0 {
+            print( "\(numero)" + " Bingo!" + " Par")
+        } else if numero % 5 == 0 {
+            print("\(numero)" + " Bingo!" + " Impar")
+        } else if numero % 2 == 0 {
+            print("\(numero)" + " Par")
         } else {
             print("\(numero)" + " Impar")
         }
